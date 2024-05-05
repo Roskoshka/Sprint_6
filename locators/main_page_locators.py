450412da775a7512cecc01ee7174ffde683c633d
Alena Kalinka <kalina.reg@yandex.ru>
2024-05-05 16:29:46 +0300

    Remark 2 has been corrected

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -12,38 +12,26 @@ class MainPageLocators:
     # Раздел "Вопросы о важном"
     FAQ_SECTION = (By.XPATH, "//div[contains(@class, 'Home_FAQ')]")
 
-    # Вопросы:
-    # Вопрос 1: "Сколько это стоит? И как оплатить?"
-    question_1 = (By.XPATH, "//div[@id='accordion__heading-0']/parent::div")
-    # Вопрос 2: "Хочу сразу несколько самокатов! Так можно?"
-    question_2 = (By.XPATH, "//div[@id='accordion__heading-1']/parent::div")
-    # Вопрос 3: "Как рассчитывается время аренды?"
-    question_3 = (By.XPATH, "//div[@id='accordion__heading-2']/parent::div")
-    # Вопрос 4: "Можно ли заказать самокат прямо на сегодня?"
-    question_4 = (By.XPATH, "//div[@id='accordion__heading-3']/parent::div")
-    # Вопрос 5: "Можно ли продлить заказ или вернуть самокат раньше?"
-    question_5 = (By.XPATH, "//div[@id='accordion__heading-4']/parent::div")
-    # Вопрос 6: "Вы привозите зарядку вместе с самокатом?"
-    question_6 = (By.XPATH, "//div[@id='accordion__heading-5']/parent::div")
-    # Вопрос 7: "Можно ли отменить заказ?"
-    question_7 = (By.XPATH, "//div[@id='accordion__heading-6']/parent::div")
-    # Вопрос 8: "Я жизу за МКАДом, привезёте?"
-    question_8 = (By.XPATH, "//div[@id='accordion__heading-7']/parent::div")
+    # Вопросы раздела "Вопросы о важном":
+    FAQ_QUESTIONS_ITEMS = {
+        1: [By.XPATH, "//div[@id='accordion__heading-0']/parent::div"],
+        2: [By.XPATH, "//div[@id='accordion__heading-1']/parent::div"],
+        3: [By.XPATH, "//div[@id='accordion__heading-2']/parent::div"],
+        4: [By.XPATH, "//div[@id='accordion__heading-3']/parent::div"],
+        5: [By.XPATH, "//div[@id='accordion__heading-4']/parent::div"],
+        6: [By.XPATH, "//div[@id='accordion__heading-5']/parent::div"],
+        7: [By.XPATH, "//div[@id='accordion__heading-6']/parent::div"],
+        8: [By.XPATH, "//div[@id='accordion__heading-7']/parent::div"]
+    }
 
-    # Ответы:
-    # Ответ 1
-    answer_1 = (By.XPATH, "//div[@id='accordion__panel-0']")
-    # Ответ 2
-    answer_2 = (By.XPATH, "//div[@id='accordion__panel-1']")
-    # Ответ 3
-    answer_3 = (By.XPATH, "//div[@id='accordion__panel-2']")
-    # Ответ 4
-    answer_4 = (By.XPATH, "//div[@id='accordion__panel-3']")
-    # Ответ 5
-    answer_5 = (By.XPATH, "//div[@id='accordion__panel-4']")
-    # Ответ 6
-    answer_6 = (By.XPATH, "//div[@id='accordion__panel-5']")
-    # Ответ 7
-    answer_7 = (By.XPATH, "//div[@id='accordion__panel-6']")
-    # Ответ 8
-    answer_8 = (By.XPATH, "//div[@id='accordion__panel-7']")
+    # Ответы раздела "Вопросы о важном":
+    FAQ_ANSWERS_ITEMS = {
+        1: (By.XPATH, "//div[@id='accordion__panel-0']"),
+        2: (By.XPATH, "//div[@id='accordion__panel-1']"),
+        3: (By.XPATH, "//div[@id='accordion__panel-2']"),
+        4: (By.XPATH, "//div[@id='accordion__panel-3']"),
+        5: (By.XPATH, "//div[@id='accordion__panel-4']"),
+        6: (By.XPATH, "//div[@id='accordion__panel-5']"),
+        7: (By.XPATH, "//div[@id='accordion__panel-6']"),
+        8: (By.XPATH, "//div[@id='accordion__panel-7']")
+    }
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,4 +1,5 @@
 import allure
+import pytest
 
 from data import TestData
 from pages.main_page import MainPage
@@ -7,82 +8,13 @@ from conftest import driver
 
 class TestMainPageFaq:
 
-    @allure.title('Раздел "Вопросы о важном" - Вопрос 1')
-    @allure.description('При клике на вопрос 1 получчаем ответ 1')
-    def test_click_faq_question_1_text_is_expected_answer_1(self, driver):
+    @allure.title('Проверка раздела "Вопросы о важном"')
+    @allure.description('Проверка появления нужного текста при нажатии на каждую иконку развертывания в разделе')
+    @pytest.mark.parametrize("question_number, expected_answer", TestData.test_data_answers_faq_section)
+    def test_click_faq_expand_icons_text_is_expected(self, driver, question_number, expected_answer):
         main_page = MainPage(driver)
         main_page.scroll_to_element(MainPageLocators.FAQ_SECTION)
-        main_page.wait_visibility_of_element(MainPageLocators.question_1)
-        main_page.click_on_element(MainPageLocators.question_1)
-        main_page.wait_visibility_of_element(MainPageLocators.answer_1)
-        assert main_page.get_text_on_element(MainPageLocators.answer_1) == TestData.expected_answer_1
-
-    @allure.title('Раздел "Вопросы о важном" - Вопрос 2')
-    @allure.description('При клике на вопрос 2 получчаем ответ 2')
-    def test_click_faq_question_2_text_is_expected_answer_2(self, driver):
-        main_page = MainPage(driver)
-        main_page.scroll_to_element(MainPageLocators.FAQ_SECTION)
-        main_page.wait_visibility_of_element(MainPageLocators.question_2)
-        main_page.click_on_element(MainPageLocators.question_2)
-        main_page.wait_visibility_of_element(MainPageLocators.answer_2)
-        assert main_page.get_text_on_element(MainPageLocators.answer_2) == TestData.expected_answer_2
-
-    @allure.title('Раздел "Вопросы о важном" - Вопрос 3')
-    @allure.description('При клике на вопрос 3 получчаем ответ 3')
-    def test_click_faq_question_3_text_is_expected_answer_3(self, driver):
-        main_page = MainPage(driver)
-        main_page.scroll_to_element(MainPageLocators.FAQ_SECTION)
-        main_page.wait_visibility_of_element(MainPageLocators.question_3)
-        main_page.click_on_element(MainPageLocators.question_3)
-        main_page.wait_visibility_of_element(MainPageLocators.answer_3)
-        assert main_page.get_text_on_element(MainPageLocators.answer_3) == TestData.expected_answer_3
-
-    @allure.title('Раздел "Вопросы о важном" - Вопрос 4')
-    @allure.description('При клике на вопрос 4 получчаем ответ 4')
-    def test_click_faq_question_4_text_is_expected_answer_4(self, driver):
-        main_page = MainPage(driver)
-        main_page.scroll_to_element(MainPageLocators.FAQ_SECTION)
-        main_page.wait_visibility_of_element(MainPageLocators.question_4)
-        main_page.click_on_element(MainPageLocators.question_4)
-        main_page.wait_visibility_of_element(MainPageLocators.answer_4)
-        assert main_page.get_text_on_element(MainPageLocators.answer_4) == TestData.expected_answer_4
-
-    @allure.title('Раздел "Вопросы о важном" - Вопрос 5')
-    @allure.description('При клике на вопрос 5 получчаем ответ 5')
-    def test_click_faq_question_5_text_is_expected_answer_5(self, driver):
-        main_page = MainPage(driver)
-        main_page.scroll_to_element(MainPageLocators.FAQ_SECTION)
-        main_page.wait_visibility_of_element(MainPageLocators.question_5)
-        main_page.click_on_element(MainPageLocators.question_5)
-        main_page.wait_visibility_of_element(MainPageLocators.answer_5)
-        assert main_page.get_text_on_element(MainPageLocators.answer_5) == TestData.expected_answer_5
-
-    @allure.title('Раздел "Вопросы о важном" - Вопрос 6')
-    @allure.description('При клике на вопрос 6 получчаем ответ 6')
-    def test_click_faq_question_6_text_is_expected_answer_6(self, driver):
-        main_page = MainPage(driver)
-        main_page.scroll_to_element(MainPageLocators.FAQ_SECTION)
-        main_page.wait_visibility_of_element(MainPageLocators.question_6)
-        main_page.click_on_element(MainPageLocators.question_6)
-        main_page.wait_visibility_of_element(MainPageLocators.answer_6)
-        assert main_page.get_text_on_element(MainPageLocators.answer_6) == TestData.expected_answer_6
-
-    @allure.title('Раздел "Вопросы о важном" - Вопрос 7')
-    @allure.description('При клике на вопрос 7 получчаем ответ 7')
-    def test_click_faq_question_7_text_is_expected_answer_7(self, driver):
-        main_page = MainPage(driver)
-        main_page.scroll_to_element(MainPageLocators.FAQ_SECTION)
-        main_page.wait_visibility_of_element(MainPageLocators.question_7)
-        main_page.click_on_element(MainPageLocators.question_7)
-        main_page.wait_visibility_of_element(MainPageLocators.answer_7)
-        assert main_page.get_text_on_element(MainPageLocators.answer_7) == TestData.expected_answer_7
-
-    @allure.title('Раздел "Вопросы о важном" - Вопрос 8')
-    @allure.description('При клике на вопрос 8 получчаем ответ 8')
-    def test_click_faq_question_8_text_is_expected_answer_8(self, driver):
-        main_page = MainPage(driver)
-        main_page.scroll_to_element(MainPageLocators.FAQ_SECTION)
-        main_page.wait_visibility_of_element(MainPageLocators.question_8)
-        main_page.click_on_element(MainPageLocators.question_8)
-        main_page.wait_visibility_of_element(MainPageLocators.answer_8)
-        assert main_page.get_text_on_element(MainPageLocators.answer_8) == TestData.expected_answer_8
+        main_page.wait_visibility_of_element(MainPageLocators.FAQ_QUESTIONS_ITEMS[question_number])
+        main_page.click_on_element(MainPageLocators.FAQ_QUESTIONS_ITEMS[question_number])
+        main_page.wait_visibility_of_element(MainPageLocators.FAQ_ANSWERS_ITEMS[question_number])
+        assert main_page.get_text_on_element(MainPageLocators.FAQ_ANSWERS_ITEMS[question_number]) == expected_answer
