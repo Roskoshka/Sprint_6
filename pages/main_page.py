90f91ef1b1dba4c1c70d887a77bd307496d92ed1
Alena Kalinka <kalina.reg@yandex.ru>
2024-05-05 15:30:21 +0300

    Remark 1 has been corrected

--- pages/main_page.py
+++ pages/main_page.py
@@ -8,16 +8,16 @@ from pages.base_page import BasePage
 class MainPage(BasePage):
     @allure.step('Кликнуть по кнопке "Заказать" в хэдере')
     def click_on_order_button_in_header(self):
-        self.driver.find_element(*BasePageLocators.ORDER_BUTTON_IN_HEADER).click()
+        self.click_on_element(BasePageLocators.ORDER_BUTTON_IN_HEADER)
 
     @allure.step('Кликнуть по кнопке "Заказать" на странице')
     def click_on_order_button_on_page(self):
-        self.driver.find_element(*MainPageLocators.ORDER_BUTTON_IN_MAIN).click()
+        self.click_on_element(BasePageLocators.ORDER_BUTTON_IN_HEADER)
 
     @allure.step('Кликнуть по "Самокат" в лого хэдера')
     def click_header_logo_scooter(self):
-        self.driver.find_element(*BasePageLocators.HEADER_LOGO_SCOOTER).click()
+        self.click_on_element(BasePageLocators.HEADER_LOGO_SCOOTER)
 
     @allure.step('Кликнуть по "Яндекс" в лого хэдера')
     def click_header_logo_yandex(self):
-        self.driver.find_element(*BasePageLocators.HEADER_LOGO_YANDEX).click()
+        self.click_on_element(BasePageLocators.HEADER_LOGO_YANDEX)
